59f15e0d63bfaf4f95e73768ce928c2e05b246f3
Mithun Sen <devmithunsen87@gmail.com>
2024-12-12 18:59:58 +0530

    Added the nodata and loader

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Filters from "../components/Filters";
 import Header from "../components/Header";
+import Loader from "../components/Loader";
+import NoData from "../components/NoData";
 import ProductCard from "../components/ProductCard";
 import { setProducts } from "../store/slices/productsSlice";
 import { AppDispatch, RootState } from "../store/store";
@@ -22,8 +24,11 @@ const Home: React.FC = () => {
         );
         const data = await rawResponse.json();
         dispatch(setProducts(data));
+      } catch (error) {
+        console.error("Error fetching products: ", error);
+      } finally {
         setIsProductLoaded(true);
-      } catch (error) {}
+      }
     }
   }, [dispatch, isProductLoaded]);
 
@@ -35,18 +40,26 @@ const Home: React.FC = () => {
     <main className="mx-auto px-4 sm:px-6 lg:px-8">
       <Header />
       <section aria-labelledby="products-heading" className="pb-24 pt-6">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
-          {isProductLoaded && products.length > 0 && <Filters />}
-          <div className="lg:col-span-4">
-            {isProductLoaded && products.length > 0 && (
+        {!isProductLoaded && (
+          <div className="flex justify-center">
+            <Loader />
+          </div>
+        )}
+        {isProductLoaded && (
+          <>{products.length === 0 && <NoData message="No data found!" />}</>
+        )}
+        {isProductLoaded && products.length > 0 && (
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
+            <Filters />
+            <div className="lg:col-span-4">
               <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                 {products.map((product) => (
                   <ProductCard key={product.id} {...product} />
                 ))}
               </div>
-            )}
+            </div>
           </div>
-        </div>
+        )}
       </section>
     </main>
   );
